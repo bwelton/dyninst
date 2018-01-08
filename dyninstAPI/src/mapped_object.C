c15e26baae64ed14cf12b81ea853371889d880dc
Benjamin Welton <welton@cs.wisc.edu>
2018-01-08 16:46:14 -0600

    Added

--- dyninstAPI/src/mapped_object.C
+++ dyninstAPI/src/mapped_object.C
@@ -2115,6 +2115,7 @@ void mapped_object::setCallee(const block_instance *b, func_instance *f) {
 
 void mapped_object::replacePLTStub(SymtabAPI::Symbol *sym, func_instance *orig, Address newAddr) {
    // Let's play relocation games...
+   fprintf(stderr, "%s %s\n", "Looking in mapped object: ", fullName().c_str());
    vector<SymtabAPI::relocationEntry> fbt;
    bool ok = parse_img()->getObject()->getFuncBindingTable(fbt);
    if(!ok) return;
@@ -2122,7 +2123,7 @@ void mapped_object::replacePLTStub(SymtabAPI::Symbol *sym, func_instance *orig, 
    
    
    for (unsigned i = 0; i < fbt.size(); ++i) {
-      fprintf(stderr, "%s %s %s %s\n", "Name: ", fbt[i].name().c_str()," Looking for: ",sym->getMangledName().c_str());
+      fprintf(stderr, "%s %s\n", "FBT Name: ", fbt[i].name().c_str());
       if (fbt[i].name() == sym->getMangledName()) {
         fprintf(stderr,"Found fbt and name, binding plt entry\n");
          proc()->bindPLTEntry(fbt[i], codeBase(), orig, newAddr);
