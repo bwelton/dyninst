cad23c4c1704df9cdb0c3feb65f5aeed7281cfb1
Benjamin Welton <welton@cs.wisc.edu>
2018-04-08 21:16:43 -0500

    Begin refactoring to add inst frame stepper to stackwalker

--- stackwalk/src/framestepper.C
+++ stackwalk/src/framestepper.C
@@ -343,9 +343,9 @@ gcframe_ret_t DyninstInstFrameStepperImpl::getCallerFrame(const Frame &in, Frame
     // Check if the stackpointer is within 50 stack elements of the frameptr. 
     // This is protection against reading unintialized memory in cases where we are
     // walking the stack of an function compiled with an ommitted framepointer.  
-    if (uint64_t(framePtr - stackPtr) > 400) {
-      sw_printf("[%s:%u] - I am Rejecting frame because abs(FP - stackPtr) > 50 stack positions - FP: %lx , SP: %lx, DIFF: %lx, Check: %u\n",
-          FILE__, __LINE__, framePtr, stackPtr, uint64_t(abs(framePtr - stackPtr)),  (addr_width * 50));          
+    if (uint64_t(framePtr - stackPtr) >= 400) {
+      sw_printf("[%s:%u] - I am Rejecting frame because (FP - stackPtr) > 50 stack positions - FP: %lx , SP: %lx, DIFF: %lx, Check: %u\n",
+          FILE__, __LINE__, framePtr, stackPtr, uint64_t((framePtr - stackPtr)),  (addr_width * 50));          
       return gcf_not_me;
     }
 
