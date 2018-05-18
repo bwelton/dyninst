cc3d3be553f2842bdb4c11ad2168eb4fca826e22
Benjamin Welton <welton@cs.wisc.edu>
2018-05-18 18:14:46 -0500

    Added

--- dyninstAPI/src/binaryEdit.C
+++ dyninstAPI/src/binaryEdit.C
@@ -465,6 +465,8 @@ bool BinaryEdit::getAllDependencies(std::map<std::string, BinaryEdit*>& deps)
    while(!depends.empty())
    {
      std::string lib = depends.front();
+     fprintf(stderr, "Loading depends: %s\n", lib.c_str());
+
      if(deps.find(lib) == deps.end()) {
         std::map<std::string, BinaryEdit*> res;
         if(!openResolvedLibraryName(lib, res)) return false;
@@ -474,9 +476,11 @@ bool BinaryEdit::getAllDependencies(std::map<std::string, BinaryEdit*>& deps)
              deps.insert(*bedit_it);
              if(!bedit_it->second->getAllDependencies(deps))
              {
+              fprintf(stderr, "Returning False #1\n");
                return false;
              }
            } else {
+            fprintf(stderr, "Returning False #2\n");
              return false;
            }
          }
