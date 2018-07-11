bb6c276f4775aba00859f8b27721546b7a5b9e08
Ben Welton <welton@cs.wisc.edu>
2018-07-11 18:02:23 -0500

    Codegen liveness fixes

--- dyninstAPI/src/addressSpace.C
+++ dyninstAPI/src/addressSpace.C
@@ -1748,11 +1748,9 @@ bool AddressSpace::relocate() {
      for (auto i : modFuncs) {
         _findPower8Overlaps[i->entryBlock()->GetBlockStartingAddress()] = i;
      }
-     FuncSet temporary = modFuncs;
-     modFuncs.clear();
-     for (auto i : temporary) {
-          if (_findPower8Overlaps.find(i->entryBlock()->GetBlockStartingAddress()+0x8) == _findPower8Overlaps.end())
-            modFuncs.push_back(i);
+     for (auto i : _findPower8Overlaps) {
+        if (_findPower8Overlaps.find(i.first + 0x8) != _findPower8Overlaps.end())
+          modFuncs.erase(i.second);
      }
 
      addModifiedRegion(iter->first);
