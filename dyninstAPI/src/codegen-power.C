6185c00a3045b771408262f1c187823141cfe32e
Ben Welton <welton@cs.wisc.edu>
2018-07-13 05:25:43 -0500

    Other changes

--- dyninstAPI/src/codegen-power.C
+++ dyninstAPI/src/codegen-power.C
@@ -133,7 +133,7 @@ void insnCodeGen::generate(codeGen &gen, instruction&insn) {
   // if (_global_stack_track.get() == NULL)
   //   _global_stack_track.reset(new TrackStacktraces());
   // _global_stack_track->Insert(ss.str());
-  if (gen.currAddr() == 0xe5f88e0) {
+  if (gen.currAddr() == 0xE5F888C) {
     fprintf(stderr, "%s\n", "Hello!, whats next???? " );
     fprintf(stderr, "%08x\n", insn.asInt());
   }
